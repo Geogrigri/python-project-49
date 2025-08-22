53cca51474c91838f565d9001cd12c867343241c
Agranov Grigory <Agranovgr@gmail.com>
2025-08-22 21:10:27 +0300

    add game brain-even

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,9 @@
+from brain_games.cli import welcome_user
+from brain_games.brain_even import brain_even
+
+def main():
+        name = welcome_user()
+        brain_even(name)
+
+if __name__ == '__main__':
+        main()
