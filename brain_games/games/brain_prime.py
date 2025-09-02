2b9ebcd68b673bde4a5eca1e2c83196256e99eec
Agranov Grigory <Agranovgr@gmail.com>
2025-09-03 00:01:32 +0300

    upgrade brain-prime

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -5,17 +5,13 @@ rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number):
-	if number <= 1:
-		return False
-	if number == 2:
-		return True
-	if number % 2 == 0:
+	if number < 2:
 		return False
 
-	for i in range(3, int(math.sqrt(number)) + 1, 2):
+	for i in range(2, int(math.sqrt(number)) + 1):
 		if number % i == 0:
 			return False
-		return True
+	return True
 
 
 def generate_round():
